e6c6041da8463a84379309925f3792862c05c2fb
yonatanNilus <yonatan@nilus.com>
2025-05-03 14:59:42 +0300

    removed unused file

--- iot/algorithms/mgm_2_algorithm.py
+++
@@ -1,8 +0,0 @@
-from algorithms.base_algorithm import BaseAlgo
-
-class MGM2(BaseAlgo):
-    def __init__(self, prob: float, graph):
-        super().__init__('MGM-2', graph, prob)
-
-    def _algorithm(self):
-        pass
