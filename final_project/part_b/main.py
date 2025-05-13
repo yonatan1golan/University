8dba75e3c4aa710f63b6b54f88021847951e3288
yonatanNilus <yonatan@nilus.com>
2025-05-13 14:24:35 +0300

    added interactions functions

--- final_project/part_b/main.py
+++ final_project/part_b/main.py
@@ -88,6 +88,19 @@ def generate_features(stock_data: StockDataFetcher):
 
     return data.dropna().reset_index(drop=True)
 
+def normalize_feature_to_z_score(data: pd.DataFrame, feature: str) -> pd.Series:
+    """ normalizes the given feature to z-scores """
+    data[feature] = data[feature].astype(float)
+    return (data[feature] - data[feature].mean()) / data[feature].std()
+
+def generate_interation_feature(data: pd.DataFrame, features: list[str]) -> pd.Series:
+    """ generates interaction features for the given features, and normalizes them to z-scores """
+    normalized = [normalize_feature_to_z_score(data, feature) for feature in features]
+    interaction_df = pd.concat(normalized, axis=1)
+    interaction_series = interaction_df.prod(axis=1)
+    feature_name = "*".join(features)
+    interaction_series.name = feature_name
+    return interaction_series
 
 class Regression:
     def __init__(self, data: pd.DataFrame, period: dict, x: list, y: str):
@@ -129,10 +142,26 @@ class Regression:
         return model
     
 if __name__ == "__main__":
-    tesla_stock = StockDataFetcher(CONFIG.TESLA_TICKER)
-    processed_tesla_data = generate_features(tesla_stock)
+    tesla_stock = None
+    try:
+        tesla_stock = StockDataFetcher(CONFIG.TESLA_TICKER)
+    except: # in cases of rate limit error from yfinance
+        tesla_stock = pd.read_csv("final_project/part_b/data/tesla_stock.csv")
 
+    processed_tesla_data = generate_features(tesla_stock)
     x_columns = processed_tesla_data.columns.difference(['volume', 'csad', 'date'])
+    
+    INTERACTIONS = {
+        "pe_daily_trend_volatility": ["pe", "daily_trend", "volatility"],
+        "weekday_return_rsi": ["weekday", "return", "rsi"]
+    }
+    pe_daily_trend_volatility = generate_interation_feature(processed_tesla_data, INTERACTIONS["pe_daily_trend_volatility"])
+    weekday_return_rsi = generate_interation_feature(processed_tesla_data, INTERACTIONS["weekday_return_rsi"])
+
+    x_columns_with_interations = list(x_columns) + pe_daily_trend_volatility.name + weekday_return_rsi.name
+    processed_tesla_data_with_interactions = processed_tesla_data.copy()
+    processed_tesla_data_with_interactions[pe_daily_trend_volatility.name] = pe_daily_trend_volatility
+    processed_tesla_data_with_interactions[weekday_return_rsi.name] = weekday_return_rsi
 
     # herding_regression = Regression(
     #     processed_tesla_data,
@@ -142,10 +171,18 @@ if __name__ == "__main__":
     # )
     # herding_regression.print_regression_results()
 
-    non_herding_regression = Regression(
-        processed_tesla_data,
-        CONFIG.FIRST_NON_HERDING_PERIOD,
-        x = list(x_columns),
-        y = 'volume'
-    )
-    print(non_herding_regression.equation)
+    # non_herding_regression = Regression(
+    #     processed_tesla_data,
+    #     CONFIG.FIRST_NON_HERDING_PERIOD,
+    #     x = list(x_columns),
+    #     y = 'volume'
+    # )
+    # print(non_herding_regression.equation)
+
+    # herding_with_interactions = Regression(
+    #     processed_tesla_data,
+    #     CONFIG.FIRST_HERDING_PERIOD,
+    #     x = list(x_columns) + [f"{INTERACTIONS[i][0]}*{INTERACTIONS[i][1]}" for i in INTERACTIONS],
+    #     y = 'volume'
+    # )
+
